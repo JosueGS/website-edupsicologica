6ceb687080c5028ea6a4f8bac3f4524d7171068a
Josuegs <jgalarza097@gmail.com>
2021-11-09 23:28:37 -0500

    Pagina de inicio 90% desarrollado

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+// const btnMenu = document.querySelector('#btnmenu');
+// const navMobil = document.querySelector('#mobilnav');
+
+
+// const clickView = () =>{
+    
+//     console.log('click en boton');
+    
+//     navMobil.classList.toggle('activeNav');
+
+// };
+
+// btnMenu.addEventListener('click', clickView );
---
+++ src/js/index.js
@@ -0,0 +1,46 @@
+// const swiper = new Swiper('.swiper-container', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   speed: 1100,
+//   loop: true,
+//   simulateTouch: true,
+//   spaceBetween: 100,
+//   // autoplay: {
+//   //   delay: 6000,
+//   //   disableOnInteraction: true,
+//   // },
+//   effect:'slide',
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       // spaceBetween: 20
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       // spaceBetween: 30
+//     },
+//     // when window width is >= 640px
+//     640: {
+//       // spaceBetween: 40
+//     },
+//     992: {
+//       spaceBetween: 250
+//     }
+//   }
+// });
+
+// const spwiperRedp = new Swiper('.swiper-perfilRed', {
+//   fadeEffect: { crossFade: true },
+//   virtualTranslate: true,
+//   speed: 1800,
+//   loop: true,
+//   noSwiping:false,
+//   slidersPerView: 1,
+//   effect: "fade",
+//   // autoplay: {
+//   //   delay: 12500,
+//   //   disableOnInteraction: false,
+//   // }
+// });
